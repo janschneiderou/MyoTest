fbcd6f50a2caa4a9fb86510f1c092501b122762e
janschneiderou <jan.schneider@ou.nl>
2018-03-02 13:01:10 +0100

    first upload

---
+++ MyoTest/MainWindow.xaml.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using MyoSharp.Communication;
+using MyoSharp.Device;
+using MyoSharp.Exceptions;
+using MyoSharp.Poses;
+using System.Diagnostics;
+using System.Windows.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.Windows.Media.Media3D;
+
+namespace MyoTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        MyoManager.MyoManagerClass myoManager = new MyoManager.MyoManagerClass();
+        public ConnectorHub.ConnectorHub myConnector;
+        public bool start = false;
+        public ConnectorHub.FeedbackHub myFeedback;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            myoManager.InitMyoManagerHub(this);
+            try
+            {
+                myConnector = new ConnectorHub.ConnectorHub();
+                myConnector.init();
+                myConnector.startRecordingEvent += MyConnector_startRecordingEvent;
+                myConnector.stopRecordingEvent += MyConnector_stopRecordingEvent;
+                myConnector.sendReady();
+                setValueNames();
+
+                myFeedback = new ConnectorHub.FeedbackHub();
+                myFeedback.init();
+                myFeedback.feedbackReceivedEvent += MyFeedback_feedbackReceivedEvent;
+            }
+            catch (Exception e)
+            {
+
+            }
+
+        }
+
+        private void MyFeedback_feedbackReceivedEvent(object sender, string feedback)
+        {
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(
+                        () =>
+                        {
+                            GripLabel.Content = feedback;
+                        }));
+        }
+
+        private void setValueNames()
+        {
+            List<string> names = new List<string>();
+            names.Add("GripPressure");
+            names.Add("OrientationW");
+            names.Add("OrientationX");
+            names.Add("OrientationY");
+            names.Add("OrientationZ");
+            names.Add("Roll");
+            names.Add("Pitch");
+            names.Add("Yaw");
+            myConnector.setValuesName(names);
+        }
+
+        private void MyConnector_stopRecordingEvent(object sender)
+        {
+            start = false;
+
+        }
+
+        private void MyConnector_startRecordingEvent(object sender)
+        {
+            start = true;
+        }
+
+
+       
+        /// <summary>
+        /// Method to update the grip textbox and assign the value to gripPressure var
+        /// </summary>
+        /// <param name="g"></param>
+        public void UpdateGripPressure(Int32 g)
+        {
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(
+                        () =>
+                        {
+                            GripTxt.Text = g.ToString();
+                        }));
+        }
+
+        /// <summary>
+        /// Method to update the orientation textbox and assign the value of orientation
+        /// </summary>
+        /// <param name="ww"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        public void UpdateOrientation(float w, float x, float y, float z)
+        {
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(
+                        () =>
+                        {
+                            OrientationTxt.Text = w.ToString() + " " + x.ToString() + " " + y.ToString() + " " + z.ToString(); ;
+                        }));
+        }
+
+        /// <summary>
+        /// Method to update the grip textbox and assign the value to gripPressure var
+        /// </summary>
+        /// <param name="g"></param>
+        public void UpdateRoll(double roll)
+        {
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(
+                        () =>
+                        {
+                            RollTxt.Text = roll.ToString();
+                        }));
+        }
+
+        /// <summary>
+        /// Method to update the grip textbox and assign the value to gripPressure var
+        /// </summary>
+        /// <param name="g"></param>
+        public void UpdatePitch(double pitch)
+        {
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(
+                        () =>
+                        {
+                            PitchTxt.Text = pitch.ToString();
+                        }));
+        }
+
+        /// <summary>
+        /// Method to update the grip textbox and assign the value to gripPressure var
+        /// </summary>
+        /// <param name="g"></param>
+        public void UpdateYaw(double yaw)
+        {
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(
+                        () =>
+                        {
+                            YawTxt.Text = yaw.ToString();
+                        }));
+        }
+
+        public void UpdateGrip(string grip)
+        {
+            Application.Current.Dispatcher.Invoke(DispatcherPriority.Background, new Action(
+                        () =>
+                        {
+                            GripLabel.Content = grip;
+                        }));
+        }
+    }
+}
---
+++ MyoTest/MyoManager/MyoManagerClass.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+using MyoSharp.Communication;
+using MyoSharp.Device;
+using MyoSharp.Exceptions;
+using MyoSharp.Poses;
+using System.Net.Sockets;
+using System.Net;
+using System.Windows.Media.Media3D;
+using ConnectorHub;
+
+namespace MyoTest.MyoManager
+{
+    class MyoManagerClass
+    {
+        IChannel channel;
+        public IHub hub;
+        MainWindow mWindow;
+
+        
+
+        Socket sending_socket;
+        IPAddress send_to_address;
+        //assign default values
+        private Int32 gripEMG=0;
+        private float orientationW=0;
+        private float orientationX=0;
+        private float orientationY=0;
+        private float orientationZ=0;
+        private double myoRoll;
+        private double myoYaw;
+        private double myoPitch;
+        int frameNumber = 0;
+
+        Int32[] firstPreEmgValue = new Int32[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
+        Int32[] secPreEmgValue = new Int32[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+        public void InitMyoManagerHub(MainWindow m)
+        {
+            this.mWindow = m;
+            channel = Channel.Create(
+                ChannelDriver.Create(ChannelBridge.Create(),
+                MyoErrorHandlerDriver.Create(MyoErrorHandlerBridge.Create())));
+            hub = Hub.Create(channel);
+
+            // listen for when the Myo connects
+            hub.MyoConnected += (sender, e) =>
+            {
+                Debug.WriteLine("Myo {0} has connected!", e.Myo.Handle);
+                e.Myo.Vibrate(VibrationType.Short);
+                e.Myo.EmgDataAcquired += Myo_EmgDataAcquired;
+                e.Myo.OrientationDataAcquired += Myo_OrientationAcquired;
+                e.Myo.PoseChanged += Myo_PoseChanged;
+                e.Myo.SetEmgStreaming(true);
+            };
+
+            // listen for when the Myo disconnects
+            hub.MyoDisconnected += (sender, e) =>
+            {
+                Debug.WriteLine("Oh no! It looks like {0} arm Myo has disconnected!", e.Myo.Arm);
+                e.Myo.SetEmgStreaming(false);
+                e.Myo.EmgDataAcquired -= Myo_EmgDataAcquired;
+            };
+
+            // start listening for Myo data
+            channel.StartListening();
+
+        }
+
+        private void Myo_PoseChanged(object sender, PoseEventArgs e)
+        {
+            string grip = e.Pose.ToString();
+            mWindow.UpdateGrip(grip);
+        }
+
+        private void Myo_EmgDataAcquired(object sender, EmgDataEventArgs e)
+        {
+
+            CalculateGripPressure(e);
+            if(gripEMG>2)
+            {
+                if (mWindow.myConnector.startedByHub==true)
+                {
+                    mWindow.myConnector.sendFeedback(gripEMG.ToString());
+                }
+               
+            }
+            if(mWindow.start==true)
+            {
+                SendDataConnector();
+            }
+        }
+
+        private void SendDataConnector()
+        {
+            try
+            {
+                List<string> values = new List<string>();
+                values.Add(gripEMG.ToString());
+                values.Add(orientationW.ToString());
+                values.Add(orientationX.ToString());
+                values.Add(orientationY.ToString());
+                values.Add(orientationZ.ToString());
+                values.Add(myoRoll.ToString());
+                values.Add(myoPitch.ToString());
+                values.Add(myoYaw.ToString());
+                mWindow.myConnector.storeFrame(values);
+            }
+            catch(Exception e)
+            {
+
+            }
+            
+
+        }
+
+        private void Myo_OrientationAcquired(object sender, OrientationDataEventArgs e)
+        {
+
+            CalculateOrientation(e);
+           // SendData();
+        }
+
+
+        /// <summary>
+        /// Method to broadcast packets of data
+        /// </summary>
+        /// <param name="pressure"></param>
+        public void SendData()
+        {
+
+            sending_socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            send_to_address = IPAddress.Parse("127.0.0.1");
+            IPEndPoint sending_end_point = new IPEndPoint(send_to_address, 11002);
+
+            SocketAsyncEventArgs socketEventArg = new SocketAsyncEventArgs();
+            socketEventArg.RemoteEndPoint = sending_end_point;
+
+            string s = "{ \"sensorName\":\"Myo\",\"attributes\":[{\"attributeName\":\"GripEMG\",\"attributteValue\":\"" + gripEMG +
+                "\"},{\"attributeName\":\"orientationW\",\"attributteValue\":\"" + orientationW +
+                "\" }, { \"attributeName\":\"orientationX\", \"attributteValue\":\"" + orientationX + 
+                "\"},{\"attributeName\":\"orientationY\",\"attributteValue\":\"" + orientationY +
+                "\" },{\"attributeName\":\"orientationZ\",\"attributteValue\":\"" + orientationZ +
+                "\" },{\"attributeName\":\"myoRoll\",\"attributteValue\":\"" + myoRoll +
+                "\" },{\"attributeName\":\"myoPitch\",\"attributteValue\":\"" + myoPitch +
+                "\" },{\"attributeName\":\"myoYaw\",\"attributteValue\":\"" + myoYaw +
+                "\" }] }";
+            s = "FrameSENT:" + frameNumber + s;
+            frameNumber++;
+            byte[] send_buffer = Encoding.UTF8.GetBytes(s);
+
+            try
+            {
+                socketEventArg.SetBuffer(send_buffer, 0, send_buffer.Length);
+                sending_socket.SendToAsync(socketEventArg);
+                Debug.WriteLine("text sent");
+            }
+            catch
+            {
+                Debug.WriteLine("not initialized");
+            }
+
+            gripEMG = 0;
+            //orientationW = 0;
+            //orientationX = 0;
+            //orientationY = 0;
+            //orientationZ = 0;
+    }
+
+        /// <summary>
+        /// Iterate through each emg sensor in myo and assign 1 if the sum of the first and second frame of emg has a sum of more than 20.
+        /// else assign 0. It means that much variation(100 to -100) was observed propotional to higher tension in muscle. 
+        /// 
+        /// </summary>
+        /// <param name="e"></param>
+        void CalculateGripPressure(EmgDataEventArgs e)
+        {
+            gripEMG = 0;
+            int[] emgTension = new int[8];
+            for (int i = 0; i < 7; i++)
+            {
+                try
+                {
+                    if ((firstPreEmgValue[i] + secPreEmgValue[i] + Math.Abs(e.EmgData.GetDataForSensor(i))) <= 20)
+                    {
+                        emgTension[i] = 0;
+
+                    }
+                    else
+                    {
+                        emgTension[i] = 1;
+                    }
+
+                }
+                catch
+                {
+                    Debug.WriteLine("Myo not connceted");
+                }
+            }
+
+            //add all value from emgTension and assign it to avgTension
+            Array.ForEach(emgTension, delegate (int i) { gripEMG += i; });
+            mWindow.UpdateGripPressure(gripEMG);
+
+            try
+            {
+                for (int i = 0; i < 7; i++)
+                {
+                    secPreEmgValue[i] = firstPreEmgValue[i];
+                    firstPreEmgValue[i] = Math.Abs(e.EmgData.GetDataForSensor(i));
+                }
+            }
+            catch
+            {
+                Debug.WriteLine("No emg value");
+            }
+        }
+
+        /// <summary>
+        /// Method called upon receiving the even myodata received. It passes on the orientation data to the UpdateOrientation class in Mainwindow
+        /// </summary>
+        /// <param name="e"></param>
+        public void CalculateOrientation(OrientationDataEventArgs e)
+        {
+            orientationW = e.Orientation.W;
+            orientationX = e.Orientation.X;
+            orientationY = e.Orientation.Y;
+            orientationZ = e.Orientation.Z;
+            mWindow.UpdateOrientation(orientationW, orientationX, orientationY, orientationZ);
+
+            myoRoll = e.Roll;
+            mWindow.UpdateRoll(myoRoll);
+            myoPitch = e.Pitch;
+            mWindow.UpdatePitch(myoPitch);
+            myoYaw = e.Yaw;
+            mWindow.UpdateYaw(myoYaw);
+        }
+    }
+}
